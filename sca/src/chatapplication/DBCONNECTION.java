c764d8222be6d397886bf07025072c57246e76cb
krish <krish@venu.mshome.net>
2023-12-22 01:34:59 +0530

    Initial commit

---
+++ sca/src/chatapplication/DBCONNECTION.java
@@ -0,0 +1,34 @@
+package chatapplication;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DBCONNECTION {
+		private static final String driver_path = "com.mysql.cj.jdbc.Driver";
+		private static final String url = "jdbc:mysql://localhost:3306/chat";
+		private static final String userName = "root";
+		private static final String passWord = "root";
+
+		public  DBCONNECTION() {
+			try {
+				Class.forName(driver_path);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		public Connection getConnection() throws SQLException {
+			return DriverManager.getConnection(url, userName, passWord);
+		}
+
+		public PreparedStatement prepareStatement(String query) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
+
+
+
---
+++ sca/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module sca {
+	requires java.sql;
+}
